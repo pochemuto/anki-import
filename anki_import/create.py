f12a0047c1c4f23379f4934ae5debc0d34387bbe
Александр Крамарев <pochemuto@gmail.com>
2024-01-17 10:07:41 +0000

    add poetry

---
+++ anki_import/create.py
@@ -0,0 +1,130 @@
+from dataclasses import dataclass
+import os
+from pprint import pprint
+import re
+import anki
+from anki.collection import Collection
+from anki.exporting import AnkiPackageExporter
+from loguru import logger
+import csv
+
+article_template = re.compile(r"^(der|die|das|[esr]) ")
+plurals = re.compile(r",\s+.*$")
+
+
+@dataclass
+class Card:
+    ru: str
+    de: str
+    beispiel: str
+    example_ru: str
+
+    def uuid(self) -> str:
+        uuid = self.de
+        uuid = re.sub(article_template, "", uuid)
+        uuid = re.sub(plurals, "", uuid)
+        return uuid
+
+
+def read_csv() -> list[Card]:
+    csv_file_path = "Lernwortschatz.csv"
+    data_list = []
+
+    with open(csv_file_path, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data_obj = Card(
+                de=row["DE"],
+                ru=row["RU"],
+                beispiel=row["Beispiel"],
+                example_ru=row["Example"],
+            )
+            data_list.append(data_obj)
+
+    return data_list
+
+
+def replace_article(string):
+    pattern = re.compile(r"^(der|die|das)\s")
+    return pattern.sub(
+        lambda match: "r "
+        if match.group(1) == "der"
+        else "e "
+        if match.group(1) == "die"
+        else "s ",
+        string,
+    )
+
+
+def create_deck(cards: list[Card]):
+    # from anki.importing.csvfile import TextImporter
+
+    # Create a new collection
+    collection = Collection("collection.anki2")
+
+    # Save and export the collection to an Anki package (.apkg) file
+    deck_id = collection.decks.id("German")
+    assert deck_id is not None
+    deck = collection.decks.get(deck_id)
+
+    model = collection.models.new("German Russian Cards")
+    model["did"] = deck_id
+    model[
+        "css"
+    ] = """
+  .card {
+    font-family: arial;
+    font-size: 20px;
+    text-align: center;
+    color: black;
+    background-color: white;
+  }
+  .from {
+    font-style: italic;
+  }
+  .example_de {
+    font-size: 15px;
+    color: gray
+  }
+  """
+
+    collection.models.addField(model, collection.models.new_field("de"))
+    collection.models.addField(model, collection.models.new_field("example_de"))
+    collection.models.addField(model, collection.models.new_field("ru"))
+    collection.models.addField(model, collection.models.new_field("example_ru"))
+
+    tmpl = collection.models.new_template("de -> ru")
+    tmpl[
+        "qfmt"
+    ] = '<div class="from">{{de}}<div class="example_de">{{example_de}}</div></div>'
+    tmpl["afmt"] = "{{FrontSide}}\n\n<hr id=answer>\n\n{{ru}}"
+    collection.models.addTemplate(model, tmpl)
+    tmpl = collection.models.new_template("ru -> de")
+    tmpl["qfmt"] = "{{ru}}"
+    tmpl["afmt"] = '{{FrontSide}}\n\n<hr id=answer>\n\n<div class="from">{{de}}</div>'
+    collection.models.addTemplate(model, tmpl)
+
+    model["id"] = 3371927463  # essential for upgrade detection
+    collection.models.update(model)
+    collection.models.set_current(model)
+    collection.models.save(model)
+
+    for card in cards:
+        note = anki.notes.Note(collection, model)
+        note["de"] = replace_article(card.de)
+        note["ru"] = card.ru
+        note["example_de"] = card.beispiel
+        note["example_ru"] = card.example_ru
+
+        note.guid = card.uuid()
+        collection.addNote(note)
+
+    exporter = AnkiPackageExporter(collection)
+    exporter.exportInto("german.apkg")
+
+
+logger.info("Exporting...")
+os.chdir('/workspaces/anki-import')
+cards = read_csv()
+create_deck(cards)
+logger.info("Done.")
